295b5affa3649c861b933cab798775593d5f93a9
Carolina <carolinazapatavidal@gmail.com>
2022-10-11 16:07:52 -0300

    First Commit

---
+++ useTodo.js
@@ -0,0 +1,48 @@
+import { useReducer, useEffect } from "react";
+import {todoReducer} from './'
+
+export const useTodo = () =>{
+const initialState = [];
+
+const init =()=>{
+  return JSON.parse(localStorage.getItem('todos')) || [];
+}
+
+  const [todos, dispatch] = useReducer(todoReducer, initialState, init);
+
+  useEffect(() => { // sólo se pueden grabar strings en el local storage
+    //local storage no deja la pc hasta que le digamos explícitamente
+    localStorage.setItem('todos', JSON.stringify(todos));
+  }, [todos])
+
+  const handleNewTodo = (todo) =>{
+    const action = {
+      type: '[TODO] Add Todo',
+      payload: todo
+    }
+    dispatch(action);
+  }
+
+  const handleDeleteTodo = (id) =>{
+    dispatch({
+      type: '[TODO] Remove Todo',
+      payload: id,
+    });
+  }
+
+  const handleToggleTodo = (id) =>{
+    dispatch({
+      type: '[TODO] Toggle Todo',
+      payload: id,
+    });
+  }
+  return {
+    todosCount: todos.length,
+    pendingTodosCount: todos.filter(todo=> !todo.done).length,
+    initialState,
+    handleDeleteTodo, 
+    handleNewTodo, 
+    handleToggleTodo, 
+    todos
+  }
+}
